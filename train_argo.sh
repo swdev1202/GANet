86be82ff80c851ac8221253a7944eb4e2b166b9c
Sean Seung Won Lee <swdev1202@gmail.com>
2020-02-20 14:30:52 -0800

    batch increased to 2

--- train_argo.sh
+++ train_argo.sh
@@ -1,5 +1,5 @@
 # Argoverse dataset
-CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=1 \
+CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=2 \
                 --crop_height=240 \
                 --crop_width=528 \
                 --max_disp=192 \
