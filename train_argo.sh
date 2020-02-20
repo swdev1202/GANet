0d237008536e10e8dc24d53b22b0258786b838d3
Sean Seung Won Lee <swdev1202@gmail.com>
2020-02-20 14:36:38 -0800

    batch back to 1

--- train_argo.sh
+++ train_argo.sh
@@ -1,5 +1,5 @@
 # Argoverse dataset
-CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=2 \
+CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=1 \
                 --crop_height=240 \
                 --crop_width=528 \
                 --max_disp=192 \
@@ -14,19 +14,19 @@ CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=2 \
                 --nEpochs=800 2>&1 |tee logs/log_finetune_argo.txt
 
 
-CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=4 \
-                --crop_height=240 \
-                --crop_width=1248 \
-                --max_disp=192 \
-                --thread=4 \
-                --data_path='/home/011505052/argoverse-conv-data/KITTI/object/training/' \
-                --training_list='./lists/argo_train.list' \
-                --val_list='./lists/argo_val.list' \
-                --save_path='./checkpoint/finetune2_argo' \
-                --argo=1 \
-                --shift=3 \
-                --lr=0.0001 \
-                --resume='./checkpoint/finetune_argo_epoch_800.pth' \
-                --nEpochs=8 2>&1 |tee logs/log_finetune_argo.txt
+# CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=4 \
+#                 --crop_height=240 \
+#                 --crop_width=1248 \
+#                 --max_disp=192 \
+#                 --thread=4 \
+#                 --data_path='/home/011505052/argoverse-conv-data/KITTI/object/training/' \
+#                 --training_list='./lists/argo_train.list' \
+#                 --val_list='./lists/argo_val.list' \
+#                 --save_path='./checkpoint/finetune2_argo' \
+#                 --argo=1 \
+#                 --shift=3 \
+#                 --lr=0.0001 \
+#                 --resume='./checkpoint/finetune_argo_epoch_800.pth' \
+#                 --nEpochs=8 2>&1 |tee logs/log_finetune_argo.txt
 
 exit
