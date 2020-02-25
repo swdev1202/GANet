7c5d484d2584df7b42cb9a4cf872fce070331188
Sean Seung Won Lee <swdev1202@gmail.com>
2020-02-24 23:36:34 -0800

    train GANet11 on sceneflow dataset

--- train_sceneflow.sh
+++ train_sceneflow.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=4 \
+CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=2 \
                 --crop_height=240 \
                 --crop_width=528 \
                 --max_disp=192 \
