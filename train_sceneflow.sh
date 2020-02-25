f69abe7a58db0a183a1ba0450424b285fcfe6b3e
Sean Seung Won Lee <swdev1202@gmail.com>
2020-02-24 23:11:31 -0800

    train GANet11 on sceneflow dataset

--- train_sceneflow.sh
+++ train_sceneflow.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python train.py --batchSize=16 \
                 --crop_width=528 \
                 --max_disp=192 \
                 --thread=16 \
-                --data_path='/ssd1/zhangfeihu/data/stereo/' \
+                --data_path='/home/011505052/sceneflow/' \
                 --training_list='lists/sceneflow_train.list' \
                 --save_path='./checkpoint/sceneflow' \
                 --resume='' \
