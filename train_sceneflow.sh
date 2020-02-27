be9cf4521f96212c79646bce5270e56f703168ab
Sean Seung Won Lee <swdev1202@gmail.com>
2020-02-26 23:54:10 -0800

    train ganet11 sceneflow 2 gpu

--- train_sceneflow.sh
+++ train_sceneflow.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=2 \
+CUDA_VISIBLE_DEVICES=0,1 python train.py --batchSize=6 \
                 --crop_height=240 \
                 --crop_width=528 \
                 --max_disp=192 \
@@ -6,8 +6,8 @@ CUDA_VISIBLE_DEVICES=0 python train.py --batchSize=2 \
                 --data_path='/home/011505052/sceneflow/' \
                 --training_list='lists/sceneflow_train.list' \
                 --save_path='./checkpoint/sceneflow' \
-                --resume='' \
+                --resume='/checkpoint/sceneflow_epoch_1.pth' \
                 --model='GANet11' \
-                --nEpochs=11 2>&1 |tee logs/log_train_sceneflow.txt
+                --nEpochs=11 2>&1 |tee logs/log_train_sceneflow2.txt
 
 exit
